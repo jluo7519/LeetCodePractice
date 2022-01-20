f40616034b4bff6c0e0768f0a16ce45d4bcb9fbb
Jialun Luo <hn.jialun.luo@gmail.com>
2022-01-20 00:20:11 -0800

    new

---
+++ src/LC00140.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LC00140 {
+
+    //without memo
+    public List<String> wordBreakNoMemo(String s, List<String> wordDict) {
+        if (s == null || s.length() == 0) {
+            return null;
+        }
+        List<String> result = new ArrayList<>();
+        Set<String> dict = new HashSet<>();
+        for (String word : wordDict) {
+            dict.add(word);
+        }
+        dfsNoMemo(s, dict, 0, new StringBuilder(), result);
+        return result;
+    }
+    private void dfsNoMemo(String s, Set<String> dict, int index, StringBuilder sb, List<String> result) {
+        int len = s.length();
+        if (index == len) {
+            sb.setLength(sb.length() - 1);
+            result.add(sb.toString());
+            return;
+        }
+
+        for (int i = index + 1; i <= len; i++) {
+            String substr = s.substring(index, i);
+            if (dict.contains(substr)) {
+                int sbLen = sb.length();
+                sb.append(substr + " ");
+                dfsNoMemo(s, dict, i, sb, result);
+                sb.setLength(sbLen);
+            }
+        }
+    }
+}
---
+++ src/TwoCups.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class TwoCups {
+    public static void main(String args[]) {
+        TwoCups test = new TwoCups();
+        System.out.println(test.twoCups(5,3,4));
+    }
+    //Q: given two cups of different size, can you measure n units of water? return min steps, if impossible return -1
+    public int twoCups(int big, int small, int target) {
+        if (target > big + small) return -1;
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {0, 0});
+        Set<String> visited=  new HashSet<>();
+        visited.add(0 + "#" + 0);
+        int minSteps = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                int[] cur = queue.poll();
+                List<int[]> nexts = convert(cur, big, small);
+                for (int[] next : nexts) {
+                    if (next[0] == target || next[1] == target || next[0] + next[1] == target) {
+                        return minSteps + 1;
+                    }
+                    if (visited.add(next[0] + "#" + next[1])) {
+                        queue.offer(next);
+                    }
+                }
+            }
+            minSteps++;
+        }
+        return -1;
+    }
+    private List<int[]> convert(int[] cur, int one, int two) {
+        List<int[]> nexts = new ArrayList<>();
+        //1. fill cup one
+        nexts.add(new int[] {one, cur[1]});
+        //2. dump cup one
+        nexts.add(new int[] {0, cur[1]});
+        //3. transfer from one to two
+        if (cur[1] == two) { //cup two is full倒不进不倒了
+            nexts.add(new int[] {cur[0], cur[1]});
+        } else if (cur[0] + cur[1] < two) { //one can't fill two'
+            nexts.add(new int[] {0, cur[0] + cur[1]});
+        } else { //fill two with one
+            nexts.add(new int[] {Math.max(0, cur[0] - (two - cur[1])), two});
+        }
+        //4. fill cup two
+        nexts.add(new int[] {cur[0], two});
+        //5. dump two
+        nexts.add(new int[] {cur[0], 0});
+        //6. transfer from two to one
+        if (cur[0] == one) { //one is full
+            nexts.add(new int[] {cur[0], cur[1]});
+        } else if (cur[0] + cur[1] < one) { // can't fill one with two
+            nexts.add(new int[] {cur[0] + cur[1], 0});
+        } else { // fill one with two
+            nexts.add(new int[] {one, Math.max(0, cur[1] - (one - cur[0]))});
+        }
+        return nexts;
+    }
+}
